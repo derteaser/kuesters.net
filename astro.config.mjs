0099ff9d7d7a4297bc3a3137fcacfc0fa31c6bb1
Jens Küsters <jens@kuesters.net>
2025-01-30 16:46:00 +0100

    🔄 Remove deprecated PostCSS and Tailwind configuration; update styles and layout components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
 import { defineConfig } from 'astro/config';
 import alpine from '@astrojs/alpinejs';
-import tailwind from '@astrojs/tailwind';
-import icon from "astro-icon";
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        alpine(), 
-        tailwind({
-            config: { applyBaseStyles: false },
-        }),
-        icon(),
-    ],
+    integrations: [alpine(), icon()],
     vite: {
         ssr: {
-          external: ["svgo"],
+            external: ['svgo'],
         },
+        plugins: [tailwindcss()],
     },
 });
